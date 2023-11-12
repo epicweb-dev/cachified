a0a9c368274950963526af787be65e5061aa3854
Taranveer Bains <taranveer786@gmailcom>
2023-11-11 19:07:03 -0800

    Feat: introduce redis-json cachified adapter

--- src/adapters.ts
+++ src/adapters.ts
@@ -142,3 +142,57 @@ export function redisCacheAdapter(redisCache: RedisLikeCache): Cache {
     },
   };
 }
+
+export interface RedisJsonLikeCache {
+  name?: string;
+  json: {
+    set(
+      key: string,
+      path: string,
+      value: Record<string, any>,
+    ): Promise<string | null>;
+    get(key: string): Promise<string | null>;
+    del(key: string): Promise<unknown>;
+  };
+  expireAt(key: string, timestamp: number): Promise<unknown>;
+}
+
+export function redisJsonCacheAdapter(
+  redisJsonCache: RedisJsonLikeCache,
+): Cache {
+  return {
+    name: redisJsonCache.name || 'RedisJson',
+    delete(key) {
+      return redisJsonCache.json.del(key);
+    },
+    async set(key: string, value: Record<string, any>) {
+      const ttl = totalTtl(value?.metadata);
+      const createdTime = value?.metadata?.createdTime;
+
+      // the $ is the root path of the json object
+      // so we are ovewriting the entire object at the specified "key"
+      const setOperation = await redisJsonCache.json.set(key, '$', value);
+
+      // We want the user to have
+      if (ttl > 0 && ttl < Infinity && typeof createdTime === 'number') {
+        await redisJsonCache.expireAt(
+          key,
+          Math.ceil((ttl + createdTime) / 1000),
+        );
+      }
+
+      return setOperation;
+    },
+
+    async get(key) {
+      const value = await redisJsonCache.json.get(key);
+      if (value == null) {
+        return null;
+      }
+      // TODO: determine how to not type coerce this into any
+      // We do this because in the redisCacheAdapter, the JSON.parse returns any
+      // so we will emulate the same behaviour here
+      return value as any;
+    },
+  };
+}
--- src/cachified.spec.ts
+++ src/cachified.spec.ts
@@ -15,6 +15,8 @@ import {
   RedisLikeCache,
   GetFreshValue,
   createCacheEntry,
+  redisJsonCacheAdapter,
+  RedisJsonLikeCache,
 } from './index';
 import { Deferred } from './createBatch';
 import { delay, report } from './testHelpers';
@@ -1639,6 +1641,81 @@ describe('cachified', () => {
         },
       }),
     ).rejects.toThrowErrorMatchingInlineSnapshot(`"Boom"`);
+  });
+});
+
+describe('it works with redisonjson like cache', () => {
+  const mockRedisJsonCache = {
+    json: {
+      set: jest.fn(),
+      get: jest.fn(),
+      del: jest.fn(),
+    },
+    expireAt: jest.fn(),
+  };
+  const cacheAdapter = redisJsonCacheAdapter(mockRedisJsonCache);
+
+  it('should set a value correctly without TTL', async () => {
+    mockRedisJsonCache.json.set.mockResolvedValue('OK');
+    const result = await cacheAdapter.set('key', { data: 'value' } as any);
+    expect(result).toBe('OK');
+    expect(mockRedisJsonCache.json.set).toHaveBeenCalledWith('key', '$', {
+      data: 'value',
+    });
+    expect(mockRedisJsonCache.expireAt).not.toHaveBeenCalled();
+  });
+
+  it('should set a value with TTL', async () => {
+    const ttl = 5000; // 5 seconds
+    const currentTime = Date.now();
+    mockRedisJsonCache.json.set.mockResolvedValue('OK');
+    const result = await cacheAdapter.set('key', {
+      metadata: { createdTime: currentTime, ttl },
+    } as any);
+    expect(result).toBe('OK');
+    expect(mockRedisJsonCache.expireAt).toHaveBeenCalledWith(
+      'key',
+      Math.ceil((ttl + currentTime) / 1000),
+    );
+  });
+
+  it('handles set operation failure', async () => {
+    mockRedisJsonCache.json.set.mockRejectedValue(new Error('Failed to set'));
+    await expect(
+      cacheAdapter.set('key', { data: 'value' } as any),
+    ).rejects.toThrow('Failed to set');
+  });
+
+  it('should retrieve an existing value', async () => {
+    const storedValue = { data: 'value' };
+    mockRedisJsonCache.json.get.mockResolvedValue(storedValue);
+    const result = await cacheAdapter.get('key');
+    expect(result).toEqual({ data: 'value' });
+  });
+
+  it('should return null for a non-existing key', async () => {
+    mockRedisJsonCache.json.get.mockResolvedValue(null);
+    const result = await cacheAdapter.get('nonExistingKey');
+    expect(result).toBeNull();
+  });
+
+  it('handles get operation failure', async () => {
+    mockRedisJsonCache.json.get.mockRejectedValue(new Error('Failed to get'));
+    await expect(cacheAdapter.get('key')).rejects.toThrow('Failed to get');
+  });
+
+  it('should delete a key', async () => {
+    await cacheAdapter.delete('key');
+    expect(mockRedisJsonCache.json.del).toHaveBeenCalledWith('key');
+  });
+
+  it('handles delete operation failure', async () => {
+    mockRedisJsonCache.json.del.mockRejectedValue(
+      new Error('Failed to delete'),
+    );
+    await expect(cacheAdapter.delete('key')).rejects.toThrow(
+      'Failed to delete',
+    );
   });
 });
 

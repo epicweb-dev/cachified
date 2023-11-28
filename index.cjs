e75ee89dbb70bd1f92836ad1e52382892fef0a33
Kent C. Dodds <me+github@kentcdodds.com>
2023-11-28 09:24:56 -0700

    chore: prep fo v4 deprecation publish

---
+++ index.cjs
@@ -0,0 +1,3 @@
+throw new Error(
+  'cachified has been moved to @epic-web/cachified with no breaking changes. Please uninstall cachified and install @epic-web/cachified instead then update all your imports. Learn more at: https://github.com/epicweb-dev/cachified/issues/74',
+);
--- jest.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('ts-jest').InitialOptionsTsJest} */
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  restoreMocks: true,
-  collectCoverageFrom: ['src/*.ts'],
-  coveragePathIgnorePatterns: ['src/*.spec.ts', 'src/index.ts'],
-};
